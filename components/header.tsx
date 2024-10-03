354140efae11bf6036a59c65add7155182d9d237
sundayonah <sundayonah94@gmail.com>
2024-10-03 21:39:26 +0100

    first commit

---
+++ components/header.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { X, Menu } from 'lucide-react';
+import Image from 'next/image';
+// import Logo from ""
+
+const Header = () => {
+   const menuItems = [
+      { name: 'Home', url: '/' },
+      { name: 'About Us', url: '/aboutUs' },
+      { name: 'Projects', url: '/projects' },
+      { name: 'Events', url: '/events' },
+      { name: 'Contact Us', url: '/contactUs' },
+   ];
+
+   // State to manage the mobile menu visibility
+   const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
+
+   // Function to toggle mobile menu visibility
+   const toggleMobileMenu = () => {
+      setMobileMenuVisible(!mobileMenuVisible);
+   };
+
+   return (
+      <>
+         <main className="w-full flex justify-between items-center fixed top-0 bg-[#fff] backdrop-blur-2xl shadow-lg h-20 z-20">
+            <div className="flex w-full p-4 px-8 justify-between items-center shadow-custom">
+               <Link href="/" className=" text-bold">
+                  <Image
+                     src="/assets/sustainability.jpg"
+                     width={50}
+                     height={40}
+                     alt="logo"
+                     className="rounded-md"
+                  />
+               </Link>
+               {/* <div> */}
+
+               {/* Mobile menu */}
+               {mobileMenuVisible ? (
+                  <div className="lg:hidden flex flex-col  space-y-2 bg-[#fff] p-4  absolute top-14 right-0 w-48 shadow-2xl">
+                     {menuItems.map((menu) => (
+                        <div key={menu.name}>
+                           <Link href={menu.url} className="text-[#201f54]">
+                              {menu.name}
+                           </Link>
+                        </div>
+                     ))}
+                     {/* <button className="text-white bg-[#2d30f0] px-5 py-2 rounded-md">
+                        Join Community
+                     </button> */}
+                  </div>
+               ) : (
+                  <>
+                     <div className="hidden lg:flex space-x-5 justify-center items-center">
+                        {menuItems.map((menu) => (
+                           <div key={menu.name}>
+                              <Link href={menu.url} className="text-[#201f54]">
+                                 {menu.name}
+                              </Link>
+                           </div>
+                        ))}
+                     </div>
+                  </>
+               )}
+               <button
+                  className=" lg:hidden text-gray-500 focus:outline-none"
+                  onClick={toggleMobileMenu}
+               >
+                  {mobileMenuVisible ? <X /> : <Menu />}
+               </button>
+            </div>
+            {/* </div> */}
+         </main>
+      </>
+   );
+};
+
+export default Header;
---
+++ components/hero.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const Hero = () => {
+   return (
+      <div className="">
+         <div className=" bg-blue-800 mt-20">
+            <div className="max-w-5xl mx-auto text-center py-4">
+               <h1 className="text-9xl text-white font-bold">SEF-2024</h1>
+               <span className=" text-3xl text-white font-bold ">
+                  SUATAINABLE ENERGY FORUM
+               </span>
+            </div>
+         </div>
+         <div className="max-w-5xl mx-auto text-center pt-2">
+            <span className=" text-blue-500 text-3xl font-bold ">
+               SUATAINABLE ENERGY SECURITY,
+            </span>
+            <h1 className="text-blue-950 text-3xl font-extrabold">
+               PAN-AFRICAN POLICY INITIATIVE
+            </h1>
+         </div>
+         <div className=" pt-3">
+            <h1 className="text-center text-2xl text-white font-bold bg-blue-800 py-3">
+               4th - 8th NOVEMBER 2024
+            </h1>
+         </div>
+         <div className="flex justify-center items-center space-x-3 pt-3">
+            <svg
+               xmlns="http://www.w3.org/2000/svg"
+               fill="none"
+               viewBox="0 0 24 24"
+               strokeWidth={1.5}
+               stroke="currentColor"
+               className="size-8 text-blue-800"
+            >
+               <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+               />
+               <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+               />
+            </svg>
+
+            <h1 className="text-blue-950 text-center text-2xl font-bold  py-3">
+               CENTRAL BUSINESS DISTRICT, ABUJA
+            </h1>
+         </div>
+      </div>
+   );
+};
+
+export default Hero;
